40b12cd79d710cf52bc5904e37a9e9a29182a2ad
Mike Moran <mmoran0032@gmail.com>
2017-03-03 16:43:59 -0500

    restructuring for package

---
+++ pyne/__init__.py
@@ -0,0 +1,8 @@
+
+
+from .buffer import *
+
+
+__version__ = '0.0.1'
+__author__ = 'Mike Moran'
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+with open('LICENSE', 'r') as f:
+    license = f.read()
+
+with open('wcf/__init__.py', 'r') as f:
+    data = f.read().split('\n')
+    for line in data:
+        if line.startswith('__version__'):
+            version = line.split()[-1].replace('\'', '')
+        elif line.startswith('__author__'):
+            author = ' '.join(line.split()[-2:]).replace('\'', '')
+
+setup(
+    name='pyne',
+    version=version,
+    description='Python for Nuclear Experiments',
+    long_description=readme,
+    author=author,
+    author_email='mmoran0032@gmail.com',
+    url=r'https://github.com/mmoran0032/pyne',
+    license=license,
+    packages=find_packages(exclude=('tests',))
+)
