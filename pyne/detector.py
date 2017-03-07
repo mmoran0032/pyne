2bfcfbeb57672046cff1e53f20611c2ab48dd2ca
Mike Moran <mmoran0032@gmail.com>
2017-03-07 17:45:38 -0500

    Plotting
    While looking at the calibration spectra, I noticed that a few of the
    channels showed hundreds of counts within the rightmost bin. I don't
    know if there were overflows that weren't marked as such in the stream,
    or overflows that were just dumped into the last bin. So, to ensure that
    the lowest and highest bins actually show nothing, since nothing should
    be there, just set them to zero.

--- pyne/detector.py
+++ pyne/detector.py
@@ -2,15 +2,19 @@
 
 from collections.abc import Sequence
 
+import matplotlib.pyplot as plt
 import numpy
 
 
 class Detector(Sequence):
-    def __init__(self, channels):
+    def __init__(self, channels, name=''):
+        self.name = name
         self.channels = channels
         self.bins = numpy.arange(self.channels + 1)
         self.adc = []
-        self.calibration = None
+        self.counts = None
+        self.total_counts = None
+        self.calibrated = None
 
     def __getitem__(self, index):
         return self.adc[index]
@@ -23,12 +27,34 @@ class Detector(Sequence):
 
     def convert_channels(self):
         self.counts, _ = numpy.histogram(self.adc, bins=self.bins)
+        # ensure first and last bins don't contain under/overflow
+        self.counts[:3] = 0
+        self.counts[-4:] = 0
+        self.total_counts = self.counts.sum()
+
+    def apply_calibration(self, f, *pars):
+        self.calibrated = f(self.bins, *pars)
+
+    def display(self, figsize=(8, 6)):
+        fig = plt.figure(figsize=figsize)
+        axis = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+        if self.counts is None:
+            self.convert_channels()
+        if self.calibrated is not None:
+            axis.plot(self.calibrated[1:], self.counts)
+            axis.set_xlim((0, self.calibrated[-1]))
+        else:
+            axis.plot(self.bins[1:], self.counts)
+            axis.set_xlim((0, self.bins[-1]))
+        axis.set_title(self.name)
+        return fig
 
 
 class DetectorArray(Sequence):
     def __init__(self, num_detectors, channels):
         self.number = num_detectors
-        self.detectors = [Detector(channels) for _ in range(self.number)]
+        self.detectors = [Detector(channels, 'adc_{:02d}'.format(i))
+                          for i in range(self.number)]
 
     def __getitem__(self, index):
         return self.detectors[index]
